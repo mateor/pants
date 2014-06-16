e4ba6bffd70cbd8ac6b5f02666efa2b869a7aafa
Mateor <mateorod9@gmail.com>
2014-06-16 17:52:28 -0500

    First gathering of review files
    This contains the integration example app, and targets for the first
    stage of android targets.
    
    The android_disribution simply passes on any SDK it sees declared in
    the users path. The android_resources target is acted upon by the
    aapt_gen task. This spiders down the res folder and converts the xml
    and res into an R.java class.
    
    Ideally, this should be able to be ran on the example app included.
    ATM there is some installation errors.

---
+++ src/python/pants/backend/codegen/targets/android_resources.py
@@ -0,0 +1,26 @@
+# Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import (nested_scopes, generators, division, absolute_import, with_statement,
+                        print_function, unicode_literals)
+
+from pants.base.build_manual import manual
+from pants.backend.android.targets.android_target import AndroidTarget
+
+@manual.builddict(tags=["android"])
+class AndroidResources(AndroidTarget):
+  """Generates an R class from android resource files. This allows resources to be referenced from java code"""
+
+  def __init__(self, **kwargs):
+    """
+    :param name: Name of target
+    :param resources: folder that holds the resource files #TODO: For now. Still debating resource keyword placement
+    :param dependencies: List of :class:`pants.base.target.Target` instances
+      this target depends on.
+    :type dependencies: list of targets
+    :param dict exclusives: An optional dict of exclusives tags. See CheckExclusives for details.
+    """
+
+    super(AndroidResources, self).__init__(**kwargs)
+    self.add_labels('codegen')
+    self.add_labels('aapt')
