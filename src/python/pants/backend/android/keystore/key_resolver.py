db0c791ea10f763142b6a20547be65e94aa8a9be
mateor <mateorod9@gmail.com>
2015-01-11 22:20:15 -0500

    Stashing to try a new tactic
    This is working to pull up a dict of the key definition but it
    has required that the sections be strings. I am going to try and
    make the section header the keystore name and let the
    fields be individual dicts. It will make the parsing routine
    much simpler.

--- src/python/pants/backend/android/keystore/key_resolver.py
+++ src/python/pants/backend/android/keystore/key_resolver.py
@@ -24,8 +24,17 @@ class KeyResolver(ChainedConfig):
   def resolve(cls, target):
     """Parse a target's keystore_config_file and return a list of Keystore objects."""
     # This needs to take the target's keystores and pull them from the keystore.configs.
+
+    #TODO: shorthand for homedir in BUILD files? They are supposed to be local only.
+    # I would like to have per-target config files as an option.
+    # as of now, the only answer is to put the config address in pants.ini exclusively.
+
     parser = cls.load(target.keystore_configs)
     key_defs = {}
     for key in target.keystores:
       key_defs[key] = parser.getdict('android-keystore', key)
     print(key_defs)
+    keys = []
+    for definition in key_defs:
+      key_defs[definition]
+
