204a40dd377a8dc89a6ca89f4fc1fbd2e29d163e
mateor <mateorod9@gmail.com>
2015-01-22 14:41:04 -0500

    Use TaskBase instead of rolling my own
    obvs.

--- tests/python/pants_test/android/tasks/test_sign_apk.py
+++ tests/python/pants_test/android/tasks/test_sign_apk.py
@@ -14,22 +14,34 @@ import unittest2 as unittest
 from pants.util.contextutil import temporary_file
 
 from pants.backend.android.tasks.sign_apk import SignApkTask
+from pants_test.tasks.test_base import TaskTest
 
 
-class SignApkTest(unittest.TestCase):
+class SignApkTest(TaskTest):
   """Test the package translation methods in pants.backend.android.aapt_gen."""
 
   _DEFAULT_KEYSTORE = '%(homedir)s/.pants.d/android/keystore_config.ini'
 
+  @classmethod
+  def task_type(cls):
+    return SignApkTask
+
   @contextmanager
-  def config_file(self):
+  def config_file(self,
+                  option=SignApkTask._CONFIG_SECTION,
+                  location=_DEFAULT_KEYSTORE):
     with temporary_file() as fp:
       fp.write(textwrap.dedent(
         """
         [{0}]
 
         keystore_config_location: {1}
-        """).format(SignApkTask._CONFIG_SECTION, self._DEFAULT_KEYSTORE))
+        """).format(option , location ))
       path = fp.name
       fp.close()
       yield path
+
+
+  def test_config_read(self):
+    with temporary_file() as config:
+      task = self.prepare_task()
