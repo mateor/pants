973663bd0f2ad5d26e1941eb368dce356d928274
mateor <mateorod9@gmail.com>
2015-01-23 15:28:48 -0500

    Stash changes.

--- src/python/pants/backend/android/tasks/sign_apk.py
+++ src/python/pants/backend/android/tasks/sign_apk.py
@@ -61,7 +61,7 @@ class SignApkTask(Task):
   def distribution(self):
     if self._dist is None:
       # No Java 8 for Android. I am considering max=1.7.0_50. See comment in render_args().
-      self._dist = Distribution.cached(maximum_version="1.7.0_99")
+      self._dist = Distribution.cached(maximum_version="1.7.0_99", jdk=True)
     print(self._dist)
     return self._dist
 
