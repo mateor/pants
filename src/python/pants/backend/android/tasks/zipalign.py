7dc4ddee751c49842ccbbf287f3fef559d196016
mateor <mateorod9@gmail.com>
2015-02-02 14:35:19 -0500

    Stash zipalign

--- src/python/pants/backend/android/tasks/zipalign.py
+++ src/python/pants/backend/android/tasks/zipalign.py
@@ -15,7 +15,7 @@ class Zipalign(AndroidTask):
   """Task to run zipalign, which byte-orders signed apks."""
 
   @classmethod
-  def prepare(self, options, round_manager):
+  def prepare(cls, options, round_manager):
     super(Zipalign, cls).prepare(options, round_manager)
     # Zipalign is a no-op on 'debug_apk' but requires both so as to bundle their tasks into a goal.
     round_manager.require_data('release_apk')
@@ -24,3 +24,13 @@ class Zipalign(AndroidTask):
   @staticmethod
   def is_zipaligntarget(target):
     return isinstance(target, AndroidBinary)
+
+  def __init__(self, *args, **kwargs):
+    super(Zipalign, self).__init__(*args, **kwargs)
+    self._android_dist = self.android_sdk
+
+  def execute(self):
+    print("WE are in ZIPalign!")
+
+
+    #TODO(BEFORE REVIEW: MOve the SignAPk products away from dist.) Zipalign is where we wil release.
