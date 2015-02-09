3c97a33f006c421a5ad8708698a15a08e453c5e8
Mateor <mateorod9@gmail.com>
2015-02-09 15:15:48 -0500

    Begin TestAndroidManifestParser.
    This should be a relatively easy class to test.
    
    -Famous Last Words, LLC.

--- tests/python/pants_test/android/test_android_manifest_parser.py
+++ tests/python/pants_test/android/test_android_manifest_parser.py
@@ -5,10 +5,39 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
+import textwrap
+import os
 import unittest
+from contextlib import contextmanager
+
+from pants.util.contextutil import temporary_file
 
 from pants.backend.android.android_manifest_parser import AndroidManifestParser
 
 
-class TestAndroidConfigUtil(unittest.TestCase):
+class TestAndroidManifestParser(unittest.TestCase):
   """Test the AndroidManifestParser class."""
+
+  @contextmanager
+  def android_manifest(self):
+    """Represent an android_binary target, providing a mock version of the required manifest."""
+    with temporary_file() as fp:
+      fp.write(textwrap.dedent(
+        """<?xml version="1.0" encoding="utf-8"?>
+        <manifest xmlns:android="http://schemas.android.com/apk/res/android"
+            package="com.pants.examples.hello" >
+            <uses-sdk
+                android:targetSdkVersion="19" />
+            <application >
+                <activity
+                    android:name="com.pants.examples.hello.HelloWorld" >
+                </activity>
+            </application>
+        </manifest>"""))
+      fp.close()
+      path = fp.name
+      yield path
+
+  def test_get_package_name(self):
+      with self.android_manifest() as manifest:
+        self.assertEqual(AndroidManifestParser.get_package_name(manifest), 'com.pants.examples.hello')
