54e5ad38740cf462b273674bdf37d162038a86c7
Mateor <mateorod9@gmail.com>
2015-02-09 12:40:52 -0500

    Insert some comments in ManifestParser
    I think all existing error-checking is sufficient. ManifestParser
    only makes sure that the fields it requires exist and that it can
    return its part of the contract. It is up to the classes that use
    those values to determine whether the answers fit their needs.

--- src/python/pants/backend/android/manifest_parser.py
+++ src/python/pants/backend/android/manifest_parser.py
@@ -15,9 +15,12 @@ class ManifestParser(object):
 
   # Parsing as in Android Donut's testrunner:
   # https://github.com/android/platform_development/blob/master/testrunner/android_manifest.py.
+
   @classmethod
   def get_package_name(cls, target):
     """Return the package name of the Android target."""
+    # If manifest tag or package tag is missing, aapt_gen errors and provides that info to stderr.
+    # This error catching shouldn't ever hit so the existing checks should be fine.
     tgt_manifest = parse(target.manifest).getElementsByTagName('manifest')
     if not tgt_manifest or not tgt_manifest[0].getAttribute('package'):
       raise cls.BadManifestError('There is no \'package\' attribute in manifest at: {0!r}'
@@ -27,6 +30,8 @@ class ManifestParser(object):
   @classmethod
   def get_target_sdk(cls, target):
     """Return a string with the Android package's target SDK."""
+    # If bad info is passed, the android_distribution class will raise an error
+    # since it will not be able to find the associated tool.
     tgt_manifest = parse(target.manifest).getElementsByTagName('uses-sdk')
     if not tgt_manifest or not tgt_manifest[0].getAttribute('android:targetSdkVersion'):
       raise cls.BadManifestError('There is no \'targetSdkVersion\' attribute in manifest at: {0!r}'
@@ -36,6 +41,7 @@ class ManifestParser(object):
   @classmethod
   def get_app_name(cls, target):
     """Return a string with the application name of the package, return None if not found."""
+    # This is used to provide a folder name in dist. Since it has a fallback value it should be ok.
     tgt_manifest = parse(target.manifest).getElementsByTagName('activity')
     try:
       package_name = tgt_manifest[0].getAttribute('android:name')
