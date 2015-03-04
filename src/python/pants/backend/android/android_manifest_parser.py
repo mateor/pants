fc03304b402de9366828ce1a428fbc493325b7f0
mateor <mateorod9@gmail.com>
2015-03-04 17:05:28 -0500

    Begin adding android_library and try to gather their resources.
    Not working yet...

--- src/python/pants/backend/android/android_manifest_parser.py
+++ src/python/pants/backend/android/android_manifest_parser.py
@@ -27,10 +27,12 @@ class AndroidManifestParser(object):
     """
     try:
       manifest = XmlParser.from_file(manifest_path)
-      target_sdk = manifest.get_attribute('uses-sdk', 'android:targetSdkVersion')
       package_name = manifest.get_attribute('manifest', 'package')
     except XmlParser.XmlError as e:
       raise cls.BadManifestError("AndroidManifest.xml parsing error: {}".format(e))
+    # TODO(BEFORE REVIEW) the way this was done, I prob. need to error check places since this is sometimes
+    #   mandated and sometimes not.
+    target_sdk = manifest.get_optional_attribute('uses-sdk', 'android:targetSdkVersion')
     app_name = manifest.get_optional_attribute('activity', 'android:name')
 
     return AndroidManifest(manifest.xml_path, target_sdk, package_name, app_name=app_name)
