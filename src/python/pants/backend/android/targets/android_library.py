2d385f0c5ab5eb2034b52fa8f5c66207b1ed127c
mateor <mateorod9@gmail.com>
2015-03-09 11:13:14 -0400

    Beginning of unpacked jars investigation.
    Cannot test due to some entropy in the antlr download process.

--- src/python/pants/backend/android/targets/android_library.py
+++ src/python/pants/backend/android/targets/android_library.py
@@ -5,8 +5,38 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
-from pants.backend.android.targets.android_target import AndroidTarget
-from pants.backend.jvm.targets.jar_library import JarLibrary
+import logging
 
-class AndroidLibrary(AndroidTarget):
-  """Produces an Android binary."""
+from pants.backend.android.targets.android_target import AndroidTarget
+from pants.backend.jvm.targets.import_jars_mixin import ImportJarsMixin
+from pants.base.payload import Payload
+from pants.base.payload_field import PrimitiveField
+
+
+logger = logging.getLogger(__name__)
+
+
+class AndroidLibrary(ImportJarsMixin, AndroidTarget):
+  """Generates a stub Java library from protobuf IDL files."""
+
+  def __init__(self, payload=None, imports=None, **kwargs):
+    """
+    :param list imports: List of addresses of `jar_library <#jar_library>`_
+      targets which contain .proto definitions.
+    """
+    payload = payload or Payload()
+    # TODO(Eric Ayers): The target needs to incorporate the settings of --gen-protoc-version
+    # and --gen-protoc-plugins into the fingerprint.  Consider adding a custom FingeprintStrategy
+    # into ProtobufGen to get it.
+    payload.add_fields({
+      'import_specs': PrimitiveField(imports or ())
+    })
+    super(AndroidLibrary, self).__init__(payload=payload, **kwargs)
+
+  @property
+  def imported_jar_library_specs(self):
+    """List of JarLibrary specs to import.
+
+    Required to implement the ImportJarsMixin.
+    """
+    return self.payload.import_specs
