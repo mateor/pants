e92d5d10234e5979520b6eff6ba82774a72c2a5b
mateor <mateorod9@gmail.com>
2015-04-04 23:32:54 -0400

    Library example is working
    Everything works well, this is great. I would like
    a little insteraction with things packed in the support
    jar but then I can move on to tests.

--- examples/src/android/library_example/src/com/pants/examples/pants_library/HelloLibrary.java
+++ examples/src/android/library_example/src/com/pants/examples/pants_library/HelloLibrary.java
@@ -2,7 +2,7 @@
 // Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
 // Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-package com.pants.examples.hello_library;
+package com.pants.examples.pants_library;
 
 import android.app.Activity;
 import android.content.res.Resources;
