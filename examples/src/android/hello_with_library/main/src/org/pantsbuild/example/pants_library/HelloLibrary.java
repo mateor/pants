3d563f4c298fd44ef6f9f722ca3ca16c3fa854ba
mateor <mateorod9@gmail.com>
2015-04-07 03:46:15 -0400

    Finish the rename to pantsbuild and fix the broken android packages
    All packages build and run. Moving off codegen has killed
    all the random build errors as well!

--- examples/src/android/hello_with_library/main/src/org/pantsbuild/example/pants_library/HelloLibrary.java
+++ examples/src/android/hello_with_library/main/src/org/pantsbuild/example/pants_library/HelloLibrary.java
@@ -2,7 +2,7 @@
 // Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
 // Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-package com.pants.examples.pants_library;
+package org.pantsbuild.example.pants_library;
 
 import android.app.Activity;
 import android.content.res.Resources;
