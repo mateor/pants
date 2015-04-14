d461607c4bd38b27f4b2ba9dc8d2debd0ff8efa7
mateor <mateorod9@gmail.com>
2015-04-14 12:16:10 -0400

    Unpacking of aars is working
    The jars is still a little messed up. But not much.
    I think this approach has several big UX wins. Hopefully
    it doesn't offend anyone with some of the code smell.

--- src/python/pants/backend/android/tasks/explode_aar.py
+++ src/python/pants/backend/android/tasks/explode_aar.py
@@ -8,6 +8,7 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 import os
 
 from pants.backend.android.targets.android_library import AndroidLibrary
+from pants.backend.android.targets.android_resources import AndroidResources
 from pants.backend.android.tasks.android_task import AndroidTask
 from pants.backend.jvm.targets.java_library import JavaLibrary
 from pants.backend.jvm.tasks.unpack_jars import UnpackJars
@@ -34,16 +35,16 @@ class ExplodeAar(UnpackJars):
     """Return True for AndroidLibrary targets."""
     return isinstance(target, AndroidLibrary)
 
-  def create_classes_jar_target(self, target, classes_jar):
+  def create_classes_jar_target(self, target, resource_dir):
     """Create a JarLibrary target for the classes.jar that is required to be in every .aar archive.
 
     :param list targets: A list of AndroidBinary targets.
     """
     # Prepare exactly N android jar targets where N is the number of SDKs in-play.
-    jar_url = 'file://{0}'.format(classes_jar)
+    jar_url = 'file://{0}'.format(resource_dir)
     address = SyntheticAddress(self.workdir, 'android-{0}.jar'.format(target))
     unpacked = self._unpack_jar
-    self.context.add_new_target(address, JavaLibrary, sources=[unpacked])
+    self.context.add_new_target(address, AndroidResources, resource_dir=resource_dir)
 
   def _unpack_jar(self, jar):
     pass
@@ -54,15 +55,33 @@ class ExplodeAar(UnpackJars):
     unpacked_archives = self.context.products.get('ivy_imports')   #jarmap = products[unpacked_jars]
     print("UNPACKED_ARCHIVES: ", unpacked_archives)
     for target in targets:
-      unpacked_library = unpacked_archives.get(target)
+      libraries = unpacked_archives.get(target)
+      print("LIBRARIES: ", libraries)
       outdir = os.path.join(self.workdir, target.manifest.package_name)
-      safe_mkdir(outdir)
-      print("THE CLASSPATH OBJECTS ARE: ", unpacked_library)
-      # If the library was an aar file then there is a classes.jar to inject into the target graph.
-      if 'classes.jar' in unpacked_library[0]:
-        classes_jar = os.path.join(unpacked_library[1], 'classes.jar')
-        print("WE FOUND A CLASSES.JAR", classes_jar)
+      for items in libraries:
+        for archive in libraries[items]:
+          print("HERE ARE THE ITEMS: ", archive)
+          if archive.endswith('.jar'):
+            print("EW FOUND AN JAR FILE: ", archive)
+            jar_file = os.path.join(outdir, archive)
+          elif archive.endswith('.aar'):
+            print("EW FOUND AN AAR: ", archive)
+            destination = os.path.join(outdir, archive)
+            ZIP.extract(os.path.join(items, archive), destination)
+            classes_jar = os.path.join(destination, 'classes.jar')
+            resource_dir = os.path.join(destination, 'res')
+            if os.path.isfile(classes_jar):
+              print("WE FOUND A CLASSES.JAR", classes_jar)
+            jar_file = classes_jar
+            if os.path.isfile(resource_dir):
+              print("WE FOUND A RESOURCE DIR", resource_dir)
+              self.create_classes_jar_target(resource_dir)
+
+          #safe_mkdir(outdir)
+          print("THE CLASSPATH OBJECTS ARE: ", libraries)
+          # If the library was an aar file then there is a classes.jar to inject into the target graph.
         print("INCLUDES ARE: ", target.include_patterns)
         unpack_filter = self._calculate_unpack_filter(target)
-        ZIP.extract(classes_jar, outdir, filter_func=unpack_filter)
+
+     #   ZIP.extract(jar_file, outdir, filter_func=unpack_filter)
         print("WE EXTARCTED YALL")
