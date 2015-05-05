dca28419228a94f4da660d5b762f28e78f5f5fbf
mateor <mateorod9@gmail.com>
2015-05-04 23:46:36 -0400

    Standardize the package name for android examples
    All android examples now belong to the org.pantsbuild.examples
    package. I also moved the BUILD file for the maven-style layout
    up to above main. I did this because it makes calling the various
    example apps more interchangable. Instead of having to remember
    to call hello_with_library/main:: you can just call
    hello_with_library:: like with all the other apps.

--- examples/src/android/hello/src/org/pantsbuild/examples/hello/HelloWorld.java
+++ examples/src/android/hello/src/org/pantsbuild/examples/hello/HelloWorld.java
@@ -1,16 +1,15 @@
 // coding=utf-8
-// Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
+// Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
 // Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-package org.pantsbuild.example.pants_library;
+package org.pantsbuild.examples.hello;
 
 import android.app.Activity;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.widget.TextView;
 
-public class HelloLibrary extends Activity {
+public class HelloWorld extends Activity {
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -20,10 +19,6 @@ public class HelloLibrary extends Activity {
 
     String text = getResources().getString(R.string.hello);
     textView.setText(text);
-
-    // Toy demonstration of using an android_library comprised of a jar and associated resources.
-    String greeting = getResources().getString(R.string.library_greeting);
-    textView.setText(greeting);
 
     setContentView(textView);
   }
