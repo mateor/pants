3578a5e74e86009363aaf120f3a918561fca1e8d
mateor <mateorod9@gmail.com>
2015-05-15 02:06:33 -0500

    DxCompile test is working.
    Now to create the test cases and major points of failure.

--- tests/python/pants_test/android/tasks/test_dx_compile.py
+++ tests/python/pants_test/android/tasks/test_dx_compile.py
@@ -18,6 +18,9 @@ from pants_test.android.test_android_base import TestAndroidBase, distribution
 class DxCompileTest(TestAndroidBase):
   """Test dex creation methods of pants.backend.android.tasks.DxCompile."""
 
+  JAVA_CLASSES_LOC = '.pants.d/compile/java/classes'
+  UNPACKED_LIBS_LOC = '.pants.d/unpack-jars/unpack-libs/explode-jars'
+
   @classmethod
   def task_type(cls):
     return DxCompile
@@ -28,32 +31,29 @@ class DxCompileTest(TestAndroidBase):
                      write_artifact_caches=None,
                      use_nailgun=False)
 
-  def _add_classes_to_context(self, context, target, files):
-
-    #dx_task = self.create_task(context)
-
+  def _add_classes_by_target(self, context, target, files):
+    # Create class files product to mock what is produced by the java compile phase.
     class_products = context.products.get_data('classes_by_target',
                                                lambda: defaultdict(MultipleRootedProducts))
     java_agent_products = MultipleRootedProducts()
     for class_file in files:
       self.create_file('.pants.d/unpack-jars/unpack-libs/{}'.format(class_file), '0xCAFEBABE string')
-      file_location = os.path.join(self.build_root, '.pants.d/unpack-jars/unpack-libs')
+      file_location = os.path.join(self.build_root, self.JAVA_CLASSES_LOC)
       java_agent_products.add_rel_paths(file_location, ['{}'.format(class_file)])
     class_products[target] = java_agent_products
-    print("CLASS PRODUCTS: ", class_products)
-
     return context
 
 
-  def test_gather_files(self):
-    with distribution() as dist:
-      with self.android_library() as android_library:
-        with self.android_binary(dependencies=[android_library]) as binary:
-          files = ['org.pantsbuild.example/a/b/c/Foo.class',
-                           'org.pantsbuild.example/a/b/c/Bar.class',
-                           'org.pantsbuild.example/a/b/c/Baz.class',]
-          context = self.context(target_roots=binary)
-
-          task_context = self._add_classes_to_context(context, android_library, files)
-          dx_task = self.create_task(task_context)
-          print("TASK: ", dx_task.context.products.get_data('classes_by_target').get(android_library))
+  def test_gather_binary_classes(self):
+    with self.android_library() as android_library:
+      with self.android_binary(dependencies=[android_library]) as binary:
+        files = ['org.pantsbuild-example-1.0/a/b/c/Foo.class',
+                 'org.pantsbuild-example-1.0/a/b/c/Bar.class',
+                 'org.pantsbuild-example-1.0/a/b/c/Baz.class',]
+        context = self.context(target_roots=binary)
+        task_context = self._add_classes_by_target(context, android_library, files)
+        dx_task = self.create_task(task_context)
+        filtered_classes = dx_task._gather_classes(binary)
+        for class_file in files:
+          file_path = os.path.join(self.build_root, self.JAVA_CLASSES_LOC, class_file)
+          self.assertIn(file_path, filtered_classes)
